72289e4a1cde0de10b46758370742a5522479ed2
SzumiecM <milosz.sz.m@gmail.com>
2021-04-20 19:52:35 +0200

    little refactor

--- main.py
+++ main.py
@@ -16,12 +16,15 @@ class AvatarCreationScreen(SwipeableScreen):
         self._screen_manager = sm
         self._current = 2
 
+        self.image_avatar = self.ids['image_avatar']
+
 
 class CameraScreen(SwipeableScreen):
     def __init__(self, sm=None, **kw):
         super(CameraScreen, self).__init__(**kw)
         self._screen_manager = sm
         self._current = 0
+
         self.camera = CameraCV(self)
         self.image_camera = self.ids['image_camera']
         self.image_avatar = self.ids['image_avatar']

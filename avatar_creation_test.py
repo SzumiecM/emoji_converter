6be062e675ab88cf1bfc88f8a904d15e236e7aa3
SzumiecM <milosz.sz.m@gmail.com>
2021-05-03 21:47:19 +0200

    update avatar test

--- avatar_creation_test.py
+++ avatar_creation_test.py
@@ -3,37 +3,31 @@ import numpy as np
 
 base = cv2.imread('images/man/base/b4m.png', -1)
 eyes = cv2.imread('images/man/eyes/brown/happy.png', -1)
-lips = cv2.imread('images/man/mouth/neutral.png', -1)
-# print(base.shape)
-# print(eyes.shape)
+mouth = cv2.imread('images/man/mouth/neutral.png', -1)
+hair = cv2.imread('images/man/hair/black/almost bald.png', -1)
 
-x_offset = 32
-y_offset = 28
 
-for y in range(y_offset, y_offset + eyes.shape[0]):
-    for x in range(x_offset, x_offset + eyes.shape[1]):
-        if not all(eyes[y - y_offset, x - x_offset] == [0, 0, 0, 0]):
-            base[y, x] = eyes[y - y_offset, x - x_offset]
+def apply_element(template, element, x_offset, y_offset):
+    for y in range(y_offset, y_offset + element.shape[0]):
+        for x in range(x_offset, x_offset + element.shape[1]):
+            if not all(element[y - y_offset, x - x_offset] == [0, 0, 0, 0]):
+                template[y, x] = element[y - y_offset, x - x_offset]
+    return template
 
-# x_offset = 38
-# y_offset = 48
-#
-# for y in range(y_offset, y_offset + nose.shape[0]):
-#     for x in range(x_offset, x_offset + nose.shape[1]):
-#         if not all(nose[y - y_offset, x - x_offset] == [0, 0, 0, 0]):
-#             base[y, x] = nose[y - y_offset, x - x_offset]
+base = apply_element(
+        apply_element(base, eyes, 32, 28),
+        mouth, 52, 80
+    )
 
-x_offset = 52
-y_offset = 80
-counter = 0
-for y in range(y_offset, y_offset + lips.shape[0]):
-    for x in range(x_offset, x_offset + lips.shape[1]):
-        if not all(lips[y - y_offset, x - x_offset] == [0, 0, 0, 0]):
-            base[y, x] = lips[y - y_offset, x - x_offset]
-            counter +=1
-
-print(counter)
-
+base = apply_element(
+    apply_element(
+        apply_element(
+            base, eyes, 32, 28
+        ),
+        mouth, 52, 80
+    ),
+    hair, 19, -42
+)
 
 file_name = 'test.png'
 cv2.imwrite(file_name, base)
